5c26524397abb6c1bf7e221dbae48dcd9be96c31
b14zej <120755309+b14zej@users.noreply.github.com>
2022-12-21 20:44:05 +0100

    Add files via upload

---
+++ scripts/b14_contents_feed.js
@@ -0,0 +1,51 @@
+const contentDisplay = document.getElementById("content");
+
+function fetchDefaultContnt(){
+	var content = "content/_content.json";
+	
+	fetch(content)
+	.then(response => {
+		if (!response.ok) {
+			throw new Error(`HTTP error: ${response.status}`);
+		}
+		
+		return response.json();
+	})
+	.then(data => {
+		for (var i in data) {
+			createSpace(i);
+			fetchData(data[i], i);
+		}
+	});
+};
+
+function fetchData(target, id){
+	fetch(target)
+	.then(response => response.text())
+	.then(text => {
+		updateContent(id, text);
+	})
+	.catch(error => {
+		throw new Error(`Fetch error: ${error}`);
+	});
+}
+
+
+function createSpace(id){
+	contentDisplay.innerHTML += "<div id='"+id+"'><h2>Loading...</h2></div>";
+	return;
+}
+
+function updateContent(id, content){
+	document.getElementById(id).innerHTML = content;
+}
+
+
+function updateFeedDisplay(new_text, id){
+	contentDisplay.innerHTML += "<div id='"+id+"' class='post'>"+new_text+"</div>";
+	console.log(`Created element of new ID: ${id}`);
+	return;
+};
+
+
+fetchDefaultContnt();
